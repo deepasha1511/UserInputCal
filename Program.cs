9d9a770ec4d1fe34c0ffc5bd68a889c6d9040015
Deepasha <deepasha.21bcon455@jecrcu.edu.in>
2025-04-05 10:25:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+        switch (op)
+        {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num2 != 0 ? num1 / num2 : double.NaN; break;
+            default: Console.WriteLine("Invalid operator!"); return;
+        }
+
+        Console.WriteLine($"Result: {result}");
+    }
+}
