dbf854e0c3bf89de19d30fd7aabb8a324894deeb
DEEPASHA <tech4dipas@gmail.com>
2025-04-05 10:53:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,22 +3,22 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rgd" {
-  name     = "UserInputCalRGD"
+  name     = "UserInputCalRG"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "storage" {
   name                     = "userinputcalstorage"
-  resource_group_name      = azurerm_resource_group.rgd.name
-  location                 = azurerm_resource_group.rgd.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_app_service_plan" "asp" {
   name                = "UserInputCalPlan"
-  location           = azurerm_resource_group.rgd.location
-  resource_group_name = azurerm_resource_group.rgd.name
+  location           = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   kind               = "Windows"
   reserved           = false
   sku {
@@ -29,7 +29,7 @@ resource "azurerm_app_service_plan" "asp" {
 
 resource "azurerm_app_service" "app" {
   name                = "UserInputCalApp"
-  location           = azurerm_resource_group.rgd.location
-  resource_group_name = azurerm_resource_group.rgd.name
+  location           = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
 }
